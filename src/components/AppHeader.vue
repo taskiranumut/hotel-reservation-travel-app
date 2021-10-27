dc1510c1eb2126f67ed0d72ef14e8a9a9b3386d8
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-27 09:53:23 +0300

    add AppHeader draft

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppHeader",
+  props: ["hotelDatas"],
+};
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div>
+      <h1 class="text-center display-3 my-2">App Title</h1>
+    </div>
+    <!-- TODO: Navbar -->
+  </div>
+</template>
